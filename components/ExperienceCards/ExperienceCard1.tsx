58643684241a74d087533359731e378632b123d1
Virendra <vsjethra13@gmail.com>
2023-08-12 04:26:58 -0400

    new commit

--- components/ExperienceCards/ExperienceCard1.tsx
+++ components/ExperienceCards/ExperienceCard1.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
-import Flutter from '../../public/images/Flutter.png'
-import GSuite from '../../public/images/GSuite.png'
+import Python from '../../public/images/Python.png'
 
 type Props = {}
 
@@ -24,8 +23,7 @@ export default function ExperienceCard({}: Props) {
             <h4 className='text-4xl font-light'>Teacher Assistant</h4>
             <p className='font-bold text-2xl mt-1'>McMaster University - Intro to Programming</p>
             <div className='flex space-x-2 my-2'>
-                <Image className='h-10 w-10 rounded-full' src={Flutter} alt="" />
-                <Image className='h-10 w-10 rounded-full' src={GSuite} alt="" />
+                <Image className='h-10 w-10 rounded-full' src={Python} alt="" />
             </div>
             <p className='uppercase py-5 text-gray-300'>Jan 2023 - May 2023</p>
 
